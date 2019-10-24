efbb223816f8ce2cfd3a7dcde58881c0b41d6a07
Marcos Boger <marcos.boger@usp.br>
2019-10-24 16:04:00 +0200

    Compare Tag Implemented
    Implemented function to check if object focused is the desired one

---
+++ 3D_VR_Game/Assets/Project/Scripts/Cube.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private Renderer rd;
+    public float totalTime = 2.0f;
+    private bool _gvrStatus;
+    private float _gvrTimer;
+    private bool _gazeComplete;
+    public string targetTag;
+    public bool match;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gazeComplete = false;
+        _gvrStatus = false;
+        _gvrTimer = 0;
+        rd = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_gvrStatus)
+        {
+            _gvrTimer += Time.deltaTime;
+        }
+        if (_gvrTimer > totalTime && _gazeComplete != true)
+        {
+            gazeCompleted();
+            _gazeComplete = true;
+        }
+    }
+
+    public void gvrOn()
+    {
+        _gvrStatus = true;
+    }
+
+    public void gvrOff()
+    {
+        _gvrStatus = false;
+        _gvrTimer = 0;
+        rd.material.color = Color.white;
+        _gazeComplete = false;
+    }
+
+    public void gazeCompleted()
+    {
+        rd.material.color = Color.grey;
+        match = gameObject.CompareTag(targetTag);
+        if (match)
+        {
+            Debug.Log("Match!");
+            //Do something if match object
+        }
+        else
+        {
+            Debug.Log("Not a match!");
+            //Do something else if doesn't match object
+        }
+    }
+
+}
--- 3D_VR_Game/Assets/Project/Scripts/GazeTimer.cs
+++ 3D_VR_Game/Assets/Project/Scripts/GazeTimer.cs
@@ -1,55 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class GazeTimer : MonoBehaviour
 {
-    private bool _gazeComplete;
     public Image imgGaze;
     public float totalTime = 2.0f;
-    bool gvrStatus;
-    float gvrTimer;
-
-    Renderer rd;
+    bool _gvrStatus;
+    float _gvrTimer;
 
     // Start is called before the first frame update
     void Start()
     {
+        _gvrStatus = false;
+        _gvrTimer = 0;
         imgGaze.fillAmount = 0f;
-        rd = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gvrStatus)
+        if (_gvrStatus)
         {
-            gvrTimer += Time.deltaTime;
-            imgGaze.fillAmount = gvrTimer / totalTime;
-            if(gvrTimer > totalTime && _gazeComplete != true)
-            {
-                gazeCompleted();
-                _gazeComplete = true;
-            }
+            _gvrTimer += Time.deltaTime;
+            imgGaze.fillAmount = _gvrTimer / totalTime;
         }
     }
 
     public void gvrOn()
     {
-        gvrStatus = true;
+        _gvrStatus = true;
     }
 
     public void gvrOff()
     {
-        gvrStatus = false;
-        gvrTimer = 0;
+        _gvrStatus = false;
+        _gvrTimer = 0;
         imgGaze.fillAmount = 0f;
-        _gazeComplete = false;
-        rd.material.color = Color.white;
     }
 
-    public void gazeCompleted()
-    {
-        rd.material.color = Color.grey;
-    }
 }
