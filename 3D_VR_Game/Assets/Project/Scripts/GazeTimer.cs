99d5c4fb1683a020ae3f0849c81fcf58f93a96d0
Marcos Boger <marcos.boger@usp.br>
2019-10-24 02:23:24 +0200

    Gaze Timer implemented with bug
    Gaze Timer implemented, but doesn't work perfectly on phone, still has to be fixed.

---
+++ 3D_VR_Game/Assets/Project/Scripts/GazeTimer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GazeTimer : MonoBehaviour
+{
+    private bool _gazeComplete;
+    public Image imgGaze;
+    public float totalTime = 2.0f;
+    bool gvrStatus;
+    float gvrTimer;
+
+    Renderer rd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        imgGaze.fillAmount = 0f;
+        rd = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            imgGaze.fillAmount = gvrTimer / totalTime;
+            if(gvrTimer > totalTime && _gazeComplete != true)
+            {
+                gazeCompleted();
+                _gazeComplete = true;
+            }
+        }
+    }
+
+    public void gvrOn()
+    {
+        gvrStatus = true;
+    }
+
+    public void gvrOff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgGaze.fillAmount = 0f;
+        _gazeComplete = false;
+    }
+
+    public void gazeCompleted()
+    {
+        rd.material.color = Color.red;
+    }
+}
