2fed98890d5711c7ce977f60e09664f40e9b0a9d
Marcos Boger <marcos.boger@usp.br>
2019-11-12 12:30:18 +0100

    AudioManager and new Audios
    AudioManager expanded for 1st room and new Audio Files for 1st room

--- 3D_VR_Game/Assets/Project/Scripts/AudioManager.cs
+++ 3D_VR_Game/Assets/Project/Scripts/AudioManager.cs
@@ -7,8 +7,20 @@ public class AudioManager : Singleton<AudioManager>
     public AudioSource SoundEffect;
 
     public AudioClip ChairClip;
-    public AudioClip GlassClip;
-    public AudioClip ForkClip;
+    public AudioClip PenClip;
+    public AudioClip BedClip;
+    public AudioClip BinClip;
+    public AudioClip ClockClip;
+    public AudioClip ClosetClip;
+    public AudioClip DoorClip;
+    public AudioClip GuitarClip;
+    public AudioClip LampClip;
+    public AudioClip LaptopClip;
+    public AudioClip MouseClip;
+    public AudioClip MugClip;
+    public AudioClip PhotoClip;
+    public AudioClip PlantClip;
+    
 
     public void ObjectSound(string Object)
     {
@@ -17,14 +29,69 @@ public class AudioManager : Singleton<AudioManager>
             SoundEffect.PlayOneShot(ChairClip);
         }
 
-        if (Object == "glass" && GlassClip != null)
+        if (Object == "pen" && PenClip != null)
         {
-            SoundEffect.PlayOneShot(GlassClip);
+            SoundEffect.PlayOneShot(PenClip);
         }
 
-        if (Object == "fork" && ForkClip != null)
+        if (Object == "bed" && BedClip != null)
         {
-            SoundEffect.PlayOneShot(ForkClip);
+            SoundEffect.PlayOneShot(BedClip);
+        }
+
+        if (Object == "bin" && BinClip != null)
+        {
+            SoundEffect.PlayOneShot(BinClip);
+        }
+
+        if (Object == "clock" && ClockClip != null)
+        {
+            SoundEffect.PlayOneShot(ClockClip);
+        }
+
+        if (Object == "closet" && ClosetClip != null)
+        {
+            SoundEffect.PlayOneShot(ClosetClip);
+        }
+
+        if (Object == "door" && DoorClip != null)
+        {
+            SoundEffect.PlayOneShot(DoorClip);
+        }
+
+        if (Object == "guitar" && GuitarClip != null)
+        {
+            SoundEffect.PlayOneShot(GuitarClip);
+        }
+
+        if (Object == "lamp" && LampClip != null)
+        {
+            SoundEffect.PlayOneShot(LampClip);
+        }
+
+        if (Object == "laptop" && LaptopClip != null)
+        {
+            SoundEffect.PlayOneShot(LaptopClip);
+        }
+
+        if (Object == "mouse" && MouseClip != null)
+        {
+            SoundEffect.PlayOneShot(MouseClip);
+        }
+
+        if (Object == "mug" && MugClip != null)
+        {
+            SoundEffect.PlayOneShot(MugClip);
+        }
+
+        if (Object == "photo" && PhotoClip != null)
+        {
+            SoundEffect.PlayOneShot(PhotoClip);
+        }
+
+        if (Object == "plant" && PlantClip != null)
+        {
+            SoundEffect.PlayOneShot(PlantClip);
         }
 
     }
