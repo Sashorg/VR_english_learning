e2cb40d2652e2423c3900b93cd15d50da15b6e25
Marcos Boger <marcos.boger@usp.br>
2019-11-19 00:15:17 +0100

    Main Menu
    Main Menu scene, which is not in VR mode. We can use this scene to customize configuration before entering a room.

---
+++ 3D_VR_Game/Assets/Project/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+public class GameManager : MonoBehaviour
+{
+    private bool _enabled;
+    private bool _load;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        XRSettings.LoadDeviceByName("");
+        XRSettings.enabled = false;
+        _enabled = false;
+        _load = false;
+    }
+
+    public void EnableVR()
+    {
+        XRSettings.LoadDeviceByName("Cardboard");
+        _enabled = true;
+    }
+
+    public void Update()
+    {
+        if (_load)
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (_enabled)
+        {
+            XRSettings.enabled = true;
+            _load = true;
+        }
+    }
+}
