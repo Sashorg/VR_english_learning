caec3caba954c6ac5658aebfb68184f94e26fc3e
Marcos Boger <marcos.boger@usp.br>
2019-12-02 13:15:22 +0100

    Adjusted objects
    Added box collider, event trigger and InteractableObject on every object prefab for them to interact with the gaze.

--- 3D_VR_Game/Assets/Project/Scripts/InteractableObject.cs
+++ 3D_VR_Game/Assets/Project/Scripts/InteractableObject.cs
@@ -86,8 +86,8 @@ public class InteractableObject : MonoBehaviour
     public void gazeCompleted()
     {
         AudioManager.Instance.ObjectSound(gameObject.name);
-        _gazedObjectName = gameObject.transform.GetChild(0).name;
-        _targetObjectName = ObjectHandler.objectToShow + "(Clone)";
+        _gazedObjectName = gameObject.name;
+        _targetObjectName = ObjectHandler.objectToShow;
 
         if(_gazedObjectName == _targetObjectName)
         {
